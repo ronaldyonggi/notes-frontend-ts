027b6f7e9d0cbb24e1ecbbf4a6208641086a2385
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-21 23:02:13 +0800

    set up initial notes state

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
+import { useState } from "react"
 import Note from "./components/Note"
+import { NoteType } from "./types/note"
 
 const App = () => {
-  const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
+  const initialNotes = [
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false
+    },
+    {
+      id: 3,
+      content: 'GET and POST are the most important methods of HTTP protocol',
+      important: true
+    }
+  ]
+
+  const [notes, setNotes] = useState<NoteType[]>(initialNotes);
 
   return (
     <div>
