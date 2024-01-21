f8ef9fc901c4f037b77b39551f5ff81599338a8d
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-21 22:11:40 +0800

    initial App setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import { useState } from 'react'
+import Note from "./components/Note"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true
+  }
+]
 
   return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+    </div>
   )
 }
-
 export default App
