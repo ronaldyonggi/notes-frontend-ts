1f4b12a6bb1ad192e86f9a047ed5a4e4b28e9bd4
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-23 16:56:33 +0800

    add type parameters to requests

--- src/services/notes.ts
+++ src/services/notes.ts
@@ -1,16 +1,17 @@
 import axios from "axios"
+import { Note } from "../types/note"
 const baseUrl = 'http://localhost:3001/notes'
 
 const getAll = () => {
-  return axios.get(baseUrl)
+  return axios.get<Note[]>(baseUrl)
 }
 
 const create = (newObject: object) => {
-  return axios.post(baseUrl, newObject)
+  return axios.post<Note>(baseUrl, newObject)
 }
 
 const update = (id: number, newObject: object) => {
-  return axios.put(`${baseUrl}/${id}`, newObject)
+  return axios.put<Note>(`${baseUrl}/${id}`, newObject)
 }
 
 export default {
