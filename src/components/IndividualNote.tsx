8dd919d13de73b9624970ff5948305826cb137fa
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-10 12:24:27 +0800

    add feature to delete a note

--- src/components/IndividualNote.tsx
+++ src/components/IndividualNote.tsx
@@ -1,18 +1,30 @@
 import { Note } from "../types/note";
+import noteService from '../services/notes'
 
 interface NoteProps {
   note: Note;
   toggleImportance: () => void;
+  notes: Note[]
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
 }
 
-const IndividualNote = ({ note, toggleImportance }: NoteProps) => {
+const IndividualNote = ({ note, toggleImportance, notes, setNotes }: NoteProps) => {
   const label = note.important
   ? 'make not important' : 'make important'
+
+  const handleDeleteClick = () => {
+    noteService.deleteNote(note.id)
+      .then(() => {
+        console.log(`Successfully deleted ${note.id}`)
+        setNotes(notes.filter(n => n.id !== note.id))
+      })
+  }
 
   return (
     <li>
       {note.content}
       <button onClick={toggleImportance}>{label}</button>
+      <button onClick={handleDeleteClick}>delete</button>
     </li>
   )
 }
