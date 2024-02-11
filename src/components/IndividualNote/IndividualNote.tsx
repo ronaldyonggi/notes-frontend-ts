28079d7889c5921dcce54c880dfc01eb021a41c5
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-11 14:20:06 +0800

    reorganize IndividualNote to its own dir

---
+++ src/components/IndividualNote/IndividualNote.tsx
@@ -0,0 +1,29 @@
+import { Note } from '../../types/note';
+
+interface NoteProps {
+  note: Note;
+  toggleImportance: (id: string) => void;
+  handleDelete: (id: string) => void;
+}
+
+const IndividualNote = ({
+  note,
+  toggleImportance,
+  handleDelete,
+}: NoteProps) => {
+  const label = note.important ? 'make not important' : 'make important';
+
+  const handleDeleteClick = () => {
+    handleDelete(note.id);
+  };
+
+  return (
+    <li>
+      {note.content}
+      <button onClick={() => toggleImportance(note.id)}>{label}</button>
+      <button onClick={handleDeleteClick}>delete</button>
+    </li>
+  );
+};
+
+export default IndividualNote;
