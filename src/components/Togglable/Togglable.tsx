9d09adc2d10c6b894c260c61880a783a2baf8953
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-25 12:59:51 +0800

    create Togglable component wrapper that toggles inside component's visibility

---
+++ src/components/Togglable/Togglable.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+interface TogglableProps {
+  buttonLabel: string;
+  children: JSX.Element | JSX.Element[];
+}
+
+const Togglable = (props : TogglableProps) => {
+  const [visible, setVisible] = useState(false)
+
+  // const hideWhenVisible = { display: visible ? 'none' : ''};
+  // const showWhenVisible = { display: visible ? '' : 'none'};
+
+  const toggleVisibility = () => setVisible(!visible);
+
+  // return (
+  //   <div>
+  //     <div style={hideWhenVisible}>
+  //       <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+  //     </div>
+  //     <div style={showWhenVisible}>
+  //       {props.children}
+  //       <button onClick={toggleVisibility}>cancel</button>
+  //     </div>
+  //   </div>
+  // )
+
+  return (
+    <div>
+      { !visible && 
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+      }
+      { visible && 
+        <div>
+          {props.children}
+          <button onClick={toggleVisibility}>cancel</button>
+        </div>
+      } 
+    </div>
+  )
+}
+export default Togglable
