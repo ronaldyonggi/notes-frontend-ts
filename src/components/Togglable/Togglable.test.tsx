b90cf7f184bbe6f00fb713108a6039dbde0baf21
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-27 13:05:25 +0800

    initial Togglable test setup

---
+++ src/components/Togglable/Togglable.test.tsx
@@ -0,0 +1,18 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Togglable from './Togglable'
+
+describe('<Togglable />', () => {
+  let container
+
+  beforeEach(() => {
+    container = render(
+      <Togglable buttonLabel='show...'>
+        <div className='testDiv'>
+          togglable content
+        </div>
+      </Togglable>
+    ).container
+  })
+})
