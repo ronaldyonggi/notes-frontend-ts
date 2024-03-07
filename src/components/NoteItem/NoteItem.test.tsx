2ace471aa6bd7c3f97475f153d89e52c7636d32a
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-07 16:01:17 +0800

    rename component IndividualNote to NoteItem

--- src/components/NoteItem/NoteItem.test.tsx
+++ src/components/NoteItem/NoteItem.test.tsx
@@ -1,24 +1,32 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import IndividualNote from './IndividualNote';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import IndividualNote from './NoteItem';
 import userEvent from '@testing-library/user-event';
 
 test('renders content', () => {
   const note = {
     content: 'Component testing is done with react-testing-library',
     important: true,
-    id: '21941'
+    id: '21941',
   };
 
   // mock function to simulate an event from an action
-  const mockHandler = jest.fn()
+  const mockHandler = jest.fn();
 
-  // Here toggleImportance and deleteNote button are both assigned mockHandler. Thus when the button for toggling importance or 
+  // Here toggleImportance and deleteNote button are both assigned mockHandler. Thus when the button for toggling importance or
   // deleting note is clicked, it just calls the mockHandler. It doesn't actually do anything.
-  render(<IndividualNote note={note} toggleImportance={mockHandler} deleteNote={mockHandler}/>)
+  render(
+    <IndividualNote
+      note={note}
+      toggleImportance={mockHandler}
+      deleteNote={mockHandler}
+    />
+  );
 
-  const element = screen.getByText('Component testing is done with react-testing-library');
-  screen.debug(element)
+  const element = screen.getByText(
+    'Component testing is done with react-testing-library'
+  );
+  screen.debug(element);
   expect(element).toBeDefined();
 
   /**
@@ -27,25 +35,30 @@ test('renders content', () => {
   // const { container } = render(<IndividualNote note={note} toggleImportance={mockHandler} deleteNote={mockHandler}/>)
   // const div = container.querySelector('.note');
   // expect(div).toHaveTextContent('Component testing is done with react-testing-library');
-
-})
+});
 
 test('clicking the button calls event handler once', async () => {
   const note = {
     content: 'Component testing is done with react-testing-library',
     important: true,
-    id: '21941'
+    id: '21941',
   };
 
-  const mockHandler = jest.fn()
-  render(<IndividualNote note={note} toggleImportance={mockHandler} deleteNote={mockHandler}/>)
+  const mockHandler = jest.fn();
+  render(
+    <IndividualNote
+      note={note}
+      toggleImportance={mockHandler}
+      deleteNote={mockHandler}
+    />
+  );
 
   // Starts a user session to interact with rendered component
-  const user = userEvent.setup()
+  const user = userEvent.setup();
 
-  const button = screen.getByText('make not important')
-  await user.click(button)
+  const button = screen.getByText('make not important');
+  await user.click(button);
 
   // Verify that the mock function has been called once
-  expect(mockHandler.mock.calls).toHaveLength(1)
-})
+  expect(mockHandler.mock.calls).toHaveLength(1);
+});
