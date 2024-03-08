59770de8c3d1fd9269117e02ba52c3a3520108b9
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-08 19:37:02 +0800

    add test for clicking button

--- src/components/NoteItem/NoteItem.test.tsx
+++ src/components/NoteItem/NoteItem.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import NoteItem from './NoteItem'
+import userEvent from '@testing-library/user-event'
 
 test('renders content', () => {
   const note = {
@@ -14,4 +15,25 @@ test('renders content', () => {
 
   const element = screen.getByText('Component testing is done with react-testing-library')
   expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true,
+    id: '21941',
+  };
+
+  const mockHandler = vi.fn()
+
+  render(<NoteItem note={note} toggleImportance={mockHandler} deleteNote={mockHandler} />)
+
+  // Starts a user session to interact with rendered component
+  const user = userEvent.setup();
+
+  const button = screen.getByText('make not important');
+  await user.click(button);
+
+  // Verify that the mock function has been called once
+  expect(mockHandler.mock.calls).toHaveLength(1);
 })
